54e781da64c9b269133190e3b6daed351929e5d0
clz2025-red <clz2025.red@gmail.com>
2025-04-22 12:01:47 +0900

    2025-04-22

--- src/com/javaex/ex04/Ex04.java
+++ src/com/javaex/ex04/Ex04.java
@@ -1,11 +1,24 @@
 package com.javaex.ex04;
 
+import java.util.Scanner;
+
 public class Ex04 {
 
 	public static void main(String[] args) {
 		
 		//구구단  for문 사용
+		Scanner sc = new Scanner(System.in);
+		System.out.println("단을 입력해 주세요");
+		System.out.print("단:");
 		
+		int dan = sc.nextInt();
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(dan +"*"+ i + "=" + dan*i );
+		}
+		
+		
+		sc.close();
 		
 	}
 
