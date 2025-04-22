d9682f76574835b325b7e164391337e2830f9af8
clz2025-red <clz2025.red@gmail.com>
2025-04-22 16:57:27 +0900

    2025-04-22

---
+++ src/com/javaex/ex04/Ex10.java
@@ -0,0 +1,35 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			
+			System.out.println("숫자를 입력하세요");
+			int no= sc.nextInt();
+			
+			if(no==0) {
+				break;
+			}// else를 break때문에 생략가능
+			
+			
+			if(no%3==0){
+				System.out.println("3의배수");
+			}else {
+				System.out.println("3의배수X");
+			}
+			
+			//반복문(while) 처음으로	
+		}
+		
+		System.out.println("종료");
+		sc.close();
+
+	}
+
+}
