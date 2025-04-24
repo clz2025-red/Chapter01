434fc261ea77d2833a8e69d7afe17843680eab59
clz2025-red <clz2025.red@gmail.com>
2025-04-24 14:51:46 +0900

    2025-04-24

---
+++ src/com/javaex/ex05/Ex02.java
@@ -0,0 +1,25 @@
+package com.javaex.ex05;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+	
+		//for문  6번
+		int no1;
+		int no2;
+		int no3;
+		int no4;
+		int no5;
+		int no6;
+		
+		//변수값을 아래와 같이 식으로 표현할수없다
+		for(int i=1; i<=6; i++) {
+			//no+i = (int)(Math.random()*45) + 1;  //배열
+		}
+		
+		
+		
+	
+	}
+
+}
